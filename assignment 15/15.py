39a299fc1add02fa7f7738d2d66af70687003a0f
Shubham123ssr <100044183+Shubham123ssr@users.noreply.github.com>
2022-09-20 23:58:03 +0530

    Add files via upload

---
+++ assignment 15/15.py
@@ -0,0 +1,67 @@
+#q1
+'''
+a="Hello world"
+b='this is class string '
+c="""Learning python is fun
+but only when you practice everyday """
+print (a)
+print(b)
+print(c)
+'''
+#q2
+'''
+my_str="iNeuron"
+print(my_str[:6])
+'''
+#q3
+'''
+my_str="Hello Learners"
+print(my_str[2:6])
+'''
+#q4
+'''
+a="Learning"
+b="Python"
+print(a+" "+b)
+'''
+#q5
+'''
+a="iNeuron"
+print(len(a))
+'''
+#q6
+'''
+txt = "iNeuron"[::-1]
+print(txt)
+'''
+#q7
+'''
+a="Hello world"
+b="world"
+if a.find(b)!=-1:
+    print("found")
+else:
+    print("Not found")
+'''
+#q8
+'''
+a="1234"
+if a.isalnum()!=True:
+    print("Not every element is in number ")
+else:
+    print("Every element is in number format")
+    '''
+#q9
+'''
+a="hello123"
+if a.isalpha()==True:
+    print("Every element is in character ")
+else:
+    print("Every element is not in character state ")
+'''
+#q10
+'''
+a=122
+b=str(a)
+print(type(b))
+'''
